46d34b22d90579a588c242515197b9b2f621e29e
Павел Беляев <xztipokzx05@mail.ru>
2025-02-20 16:11:37 +0300

    Добавлен класс Animals, для лучшей работы с животными

--- Program.cs
+++ Program.cs
@@ -44,55 +44,10 @@ namespace Intertfaces
         public static void MakeHunt(IWildAnimal wildAnimal) { wildAnimal.Hunt(); } //метод, который инициализирует метод охоты у дикого животного
     }
 
-    //В C# запрещено наследование более чем от 1 класса, поэтому существуют интерфейсы, который представляет из себя блок абстрактных членов
-    //(в основном методов). Вызывается как interface IНазваниеИнтерфейса {//абстрактные члены} (модификато доступа не нужен, всегда public)
+    
 
-    abstract class Animal
-    {
-        public abstract void Speak();
-        public abstract void Move();
-    }
-    interface IDomesticAnimal //интерфейст, характеризующий поведение домашних животных
-    {
-        void Eat();
-        void GetLazy();
-    }
-    interface IWildAnimal
-    {
-        void Hunt();
-        void Fight();
-    }
-    class Cat : Animal, IDomesticAnimal
-    {
-        public override void Speak() => Console.WriteLine("Мяу!");
-        public override void Move() => Console.WriteLine("Перебирает лапками");
-        public void Eat() => Console.WriteLine("Ест сухарики"); //не нужно добавлять override для абстрактно-виртуальных методов интерфейса
-        public void GetLazy() => Console.WriteLine("Кот опять уснул");
-    }
-    class Dog : Animal, IDomesticAnimal
-    {
-        public override void Speak() => Console.WriteLine("Гав!");
-        public override void Move() => Console.WriteLine("Пес-бегун побежал");
-        public void Eat() => Console.WriteLine("Ест кость");
-        public void GetLazy() => Console.WriteLine("Пес спит");
-    }
-    class Fox : Animal, IWildAnimal
-    {
-        public override void Speak() => Console.WriteLine("Фыр фыр фыр!");
-        public override void Move() => Console.WriteLine("Лисица-куница побежала");
-        public void Hunt() => Console.WriteLine("Выслеживает колобка или что-нибудь покрупнее");
-        public void Fight() => Console.WriteLine("Это моя норка-шконка!");
-    }
-    class Bear : Animal, IWildAnimal
-    {
-        public override void Speak() => Console.WriteLine("Рык на всю округу!");
-        public override void Move() => Console.WriteLine("Косолапый прибыл");
-        public void Hunt() => Console.WriteLine("Щас бы медка");
-        public void Fight() => Console.WriteLine("Тебе с левой лапы, с правой или с обеих?");
-    }
-    //создадим свой зоопарк, в котором будут только дикие животные, на данный момент имеются только лисы и медведи, но если появятся другие дикие звери
-    //нужно будет редактировать зоопарк, чтобы такого не делать, существует инверсия зависимостей, которая позволяет устранить связи между подсистемами
-    //путем установления их общей зависимости от абстракции - интерфейса:
+  
+  
   
 
 }
